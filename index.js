31d7742e9608a956d903cf52df5380d3fd9d74e0
onusko228 <alex2312cool@gmail.com>
2023-07-02 13:59:17 +0300

    adaptive bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let menuBtn = document.querySelector('.menu-btn');
-let menu = document.querySelector('.nav-list');
-let menuItem = document.querySelectorAll('.nav-link');
+let menu = document.querySelector('.header__list');
+let menuItem = document.querySelectorAll('.header__item');
 
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
@@ -8,8 +8,8 @@ menuBtn.addEventListener('click', function(){
 })
 
 
-menuItem.forEach (function(menuItem) {
-  menuItem.addEventListener('click',function(){
+menuItem.forEach (function(item) {
+  item.addEventListener('click',function(){
           menuBtn.classList.toggle('active');
           menu.classList.toggle('active');
   })
